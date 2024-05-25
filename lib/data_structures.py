ae71a0954c55d55df0e4ed3b41262859eada53e9
Charity <charitywanjiku8245@gmail.com>
2024-05-26 00:06:34 +0300

    Complete lab

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,65 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
+    name_list = []
+    i = 0
+    while(i < len(spicy_foods)):
+        name_list.append(spicy_foods[i].get('name'))
+        i += 1
+    return name_list   
     pass
 
 def get_spiciest_foods(spicy_foods):
+    spiciest_foods = []
+    i = 0
+    while(i < len(spicy_foods)):
+        if(spicy_foods[i].get('heat_level') > 5):
+            spiciest_foods.append(spicy_foods[i])
+        i += 1
+    return spiciest_foods    
     pass
 
 def print_spicy_foods(spicy_foods):
+    for food in spicy_foods:
+        print( f"{food.get('name')} ({food.get('cuisine')}) | Heat Level: {'🌶' * food.get('heat_level')}")
     pass
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
+    i = 0
+    while(i < len(spicy_foods)):
+        if(spicy_foods[i].get('cuisine') == cuisine):
+            return spicy_foods[i]
+        i += 1
+    return None    
     pass
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    spiciest_foods = []
+    i = 0
+    while(i < len(spicy_foods)):
+        if(spicy_foods[i].get('heat_level') > 5):
+            spiciest_foods.append(spicy_foods[i])
+        i += 1
+    for food in spiciest_foods:
+        print( f"{food.get('name')} ({food.get('cuisine')}) | Heat Level: {'🌶' * food.get('heat_level')}")
+    pass    
 
 def get_average_heat_level(spicy_foods):
+    total = 0
+    for food in spicy_foods:
+        total += food.get('heat_level')
+    return total/len(spicy_foods)
     pass
 
 def create_spicy_food(spicy_foods, spicy_food):
+    spicy_foods.append(spicy_food)
+    return spicy_foods
     pass
+print(create_spicy_food(
+    spicy_foods,
+    {
+        'name': 'Griot',
+        'cuisine': 'Haitian',
+        'heat_level': 10,
+    }
+))
